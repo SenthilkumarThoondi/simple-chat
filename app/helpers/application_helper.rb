5b60289d2bb8906e57e66a78340e9e1cfefa3320
SenthilkumarThoondi <senthilkumarthoondi@gmail.com>
2025-03-27 10:32:53 +0530

    rubocop warnings fixed for app directory

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# this helper for general application related methods
 module ApplicationHelper
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# this model is parent for all models
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 end
