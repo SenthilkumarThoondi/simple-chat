26ab2eea7e836e1eddfdebfeb4560d9d21b120b5
SenthilkumarThoondi <senthilkumarthoondi@gmail.com>
2025-03-29 10:49:32 +0530

    message form added.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,11 +6,25 @@ class MessagesController < ApplicationController
   before_action :set_chat
   def index
     @messages = @chat.messages
+    @message = Message.new
+  end
+
+  def create
+    @message = @chat.messages.new(message_params)
+    if @message.save
+      redirect_to chat_messages_path(@chat)
+    else
+      render :index
+    end
   end
 
   private
 
   def set_chat
     @chat = Chat.find(params[:chat_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:content, :sender_id)
   end
 end
